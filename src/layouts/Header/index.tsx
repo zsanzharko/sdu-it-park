099449608342156fb245de131d1b988b378b1528
sch-aa-vk <sch.aa.vk@gmail.com>
2023-04-14 01:43:42 +0600

    fix: add underline to header links

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,20 +1,34 @@
-import { Link } from 'react-router-dom';
-
-import './style.scss';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { routes } from './index.routes';
 import { Logo } from '../../components/Logo';
 
+import './style.scss';
+
 export const Header: React.FC = () => {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(-1);
+  useEffect(() => {
+    routes.forEach((route) => {
+      if (route.link === location.pathname) setActiveLink(route.id);
+      else if (location.pathname === '/') setActiveLink(-1);
+    });
+  }, [location]);
+
   return (
     <div className="header">
       <Logo />
       <nav className="header__links">
-        {routes.map((route) => (
-          <Link key={route.id} className="header__link" to={route.link}>
-            {route.title}
-          </Link>
-        ))}
+        {routes.map((route) => {
+          const className =
+            route.id === activeLink ? 'header__link header__link-active' : 'header__link';
+          return (
+            <Link key={route.id} className={className} to={route.link}>
+              {route.title}
+            </Link>
+          );
+        })}
       </nav>
     </div>
   );
