db44cd354ce87b5526c2a33ea1acd9fb9111ca66
zsanzharko <semid46@gmail.com>
2023-04-24 12:38:14 +0600

    fixed filename, and fixed in index adding lib React

--- src/layouts/OurTeam/index.tsx
+++ src/layouts/OurTeam/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { DecorationArrow } from '../../components/DecorationArrow';
 import { PageTitle } from '../../components/PageTitle';
