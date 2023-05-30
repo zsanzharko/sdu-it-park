0ea2e7ddb22d47b47df125a6ff96157ec45ad6df
sch-aa-vk <sch.aa.vk@gmail.com>
2023-05-30 21:13:53 +0600

    feat: add page404

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { Actual } from './pages/Actual';
 import { Cooperation } from './pages/Cooperation';
+import { Page404 } from './pages/Page404';
 
 export const App: React.FC = () => {
   return (
@@ -17,6 +18,7 @@ export const App: React.FC = () => {
         <Route path="/actual" element={<Actual />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cooperation" element={<Cooperation />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/Page404/index.tsx
@@ -0,0 +1,10 @@
+import './style.scss';
+
+export const Page404 = () => {
+  return (
+    <div className="page-undefined">
+      <p className="page-undefined__text-large">404</p>
+      <p className="page-undefined__text">К сожалению страница не найдена</p>
+    </div>
+  );
+};
